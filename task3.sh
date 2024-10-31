008fd6de0c6a08cfebd06185fc099904fc7d5ef6
Ansh Ganatra <32770005+anshganatra@users.noreply.github.com>
2024-10-30 23:15:11 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,1 +1,1 @@
-gawk -F',' '$3 == 2 && $NF ~ /S/ {print}' titanic.csv | sed 's/male/M/; s/female/F/' | gawk -F',' '{if ($7 != "") {ageSum += $7; ageCount++}} END {print "Average age:", ageSum / ageCount}'
+gawk -F',' '$3 == 2 && $NF ~ /S/ {print}' titanic.csv | sed 's/male/M/; s/female/F/' | gawk -F',' '{print; if ($7 != "") {ageSum += $7; ageCount++}} END {print "\nAverage age:", ageSum / ageCount}'
